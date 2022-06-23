d8ce42f067891c973a12b209c7d31290f64c2cd9
LooSheng <loosheng@outlook.com>
2022-06-23 22:10:01 +0800

    chore: type check error

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { franc, francAll } from '..'
+import { franc, francAll } from '../src'
 
 describe('franc tests', () => {
   it('franc', () => {
